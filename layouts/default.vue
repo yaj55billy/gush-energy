27d3ef9a03c1e0c69b1ad18651b7ee839dc4ca68
yaj55billy <yaj55ushydna@gmail.com>
2023-11-06 10:48:25 +0800

    case init

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div>
+		<!-- <NuxtLoadingIndicator
+			color="#fdc23e"
+			:height="5"
+			:duration="4000"
+			:throttle="0"
+		/> -->
+		<!-- <Header /> -->
+		<slot />
+		<!-- <Footer /> -->
+	</div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+// import xxx from "@/components/xxx"
+</script>
+
+<template>
+	<div class="index">
+		<h1>湧業</h1>
+		<h3>最新消息</h3>
+	</div>
+</template>
+
+<style scoped></style>
