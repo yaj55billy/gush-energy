44dae16f68319cc98f66667c2b6be4c2136125a4
yaj55billy <yaj55ushydna@gmail.com>
2023-12-15 21:32:45 +0800

    service page(30%)

---
+++ components/ServiceMenu.vue
@@ -0,0 +1,62 @@
+<script setup>
+const route = useRoute();
+console.log(route.name);
+if (route.name === "index") {
+	console.log("首頁");
+}
+if (route.name === "service") {
+	console.log("服務項目");
+}
+</script>
+
+<template>
+	<ul class="service__list">
+		<li class="service__item">
+			<div class="service__itempic">
+				<img src="~/assets/img/index_service_icon1.png" alt="申設流程" />
+			</div>
+			<div class="service__itemtextarea">
+				<h3 class="service__itemtitle">申設流程</h3>
+			</div>
+			<NuxtLink to="/service" class="service__itemlink"></NuxtLink>
+		</li>
+		<li class="service__item">
+			<div class="service__itempic">
+				<img src="~/assets/img/index_service_icon2.png" alt="光電工程EPC" />
+			</div>
+			<div class="service__itemtextarea">
+				<h3 class="service__itemtitle">光電工程EPC</h3>
+			</div>
+			<NuxtLink to="/service" class="service__itemlink"></NuxtLink>
+		</li>
+		<li class="service__item">
+			<div class="service__itempic">
+				<img src="~/assets/img/index_service_icon3.png" alt="施工維運" />
+			</div>
+			<div class="service__itemtextarea">
+				<h3 class="service__itemtitle">施工維運</h3>
+			</div>
+			<NuxtLink to="/service" class="service__itemlink"></NuxtLink>
+		</li>
+		<li class="service__item">
+			<div class="service__itempic">
+				<img src="~/assets/img/index_service_icon4.png" alt="電廠投資" />
+			</div>
+			<div class="service__itemtextarea">
+				<h3 class="service__itemtitle">電廠投資</h3>
+			</div>
+			<NuxtLink to="/service" class="service__itemlink"></NuxtLink>
+		</li>
+		<li class="service__item">
+			<div class="service__itempic">
+				<img src="~/assets/img/index_service_icon5.png" alt="儲能系統" />
+			</div>
+			<div class="service__itemtextarea">
+				<h3 class="service__itemtitle">儲能系統</h3>
+			</div>
+			<NuxtLink to="/service" class="service__itemlink"></NuxtLink>
+		</li>
+	</ul>
+</template>
+
+<style></style>
--- pages/service.vue
+++ pages/service.vue
@@ -1,33 +1,8 @@
 <script setup>
-import { useAssetHandle } from "@/composables/useAssetHandle.js";
+// import { useAssetHandle } from "@/composables/useAssetHandle.js";
 
-const { useAsset } = useAssetHandle();
-
-// const serviceApplyList = ref(null);
-// const serviceApplyItem = ref(null);
-// const serviceApplyLineWidth = ref(0);
-// const serviceApplyLineHeight = ref(0);
-
-// const cal = () => {
-// 	// temp
-// 	serviceApplyLineWidth.value =
-// 		serviceApplyList.value?.clientWidth -
-// 		serviceApplyItem.value?.clientWidth +
-// 		"px";
-
-// 	serviceApplyLineHeight.value =
-// 		serviceApplyList.value?.clientHeight -
-// 		serviceApplyItem.value?.clientHeight +
-// 		"px";
-// };
-
-onMounted(() => {
-	// cal();
-	// window.addEventListener("resize", cal);
-});
-onUnmounted(() => {
-	// window.removeEventListener("resize", cal);
-});
+// const { useAsset } = useAssetHandle();
+const x = 2;
 </script>
 
 <template>
@@ -37,8 +12,11 @@ onUnmounted(() => {
 			text="湧業為您提供專業的光電整合服務，伴您與綠能未來同行"
 			picPath="qa_kv.jpg"
 		/>
+
 		<section class="service__info">
-			<div class="service__info__container"></div>
+			<div class="service__info__container">
+				<ServiceMenu></ServiceMenu>
+			</div>
 		</section>
 		<section class="service__epc">
 			<div class="service__epc__container">
@@ -53,24 +31,8 @@ onUnmounted(() => {
 
 		<section class="service__apply">
 			<div class="service__apply__container">
-				<h2 class="page__title">申請流程</h2>
+				<h2 class="page__title">申設流程</h2>
 				<div class="service__apply__content">
-					<svg
-						class="service__apply__line"
-						ref="serviceApplyLine"
-						xmlns="http://www.w3.org/2000/svg"
-						width="405"
-						height="401"
-						viewBox="0 0 405 401"
-						fill="none"
-					>
-						<path
-							d="M1 0.5H392C398.627 0.5 404 5.87258 404 12.5V98.5C404 105.127 398.627 110.5 392 110.5H13C6.37259 110.5 1 115.873 1 122.5V190C1 196.627 6.37258 202 13 202H392C398.627 202 404 207.373 404 214V292.5C404 299.127 398.627 304.5 392 304.5H13C6.37259 304.5 1 309.873 1 316.5V388C1 394.627 6.37258 400 13 400H404"
-							stroke="white"
-							stroke-opacity="0.35"
-							stroke-dasharray="4 4"
-						/>
-					</svg>
 					<ul class="service__apply__list" ref="serviceApplyList">
 						<li class="service__apply__item" ref="serviceApplyItem">
 							<div class="service__apply__body">
@@ -81,7 +43,6 @@ onUnmounted(() => {
 									/>
 								</div>
 								<span class="service__apply__num">1</span>
-								<span class="service__apply__start">START</span>
 							</div>
 							<h5 class="service__apply__head">現地勘查</h5>
 						</li>
@@ -103,33 +64,7 @@ onUnmounted(() => {
 									<img src="~/assets/img/service_apply_icon3.png" alt="簽約" />
 								</div>
 								<span class="service__apply__num">3</span>
-								<svg
-									class="service__apply__arrow"
-									xmlns="http://www.w3.org/2000/svg"
-									width="33"
-									height="22"
-									viewBox="0 0 33 22"
-									fill="none"
-								>
-									<path
-										d="M33 1.58349e-06L27.2359 0.329821L30.4036 5.15675L33 1.58349e-06ZM1.27433 21.418L2.87433 20.368L2.32567 19.532L0.725672 20.582L1.27433 21.418ZM6.07433 18.268L9.27433 16.168L8.72567 15.332L5.52567 17.432L6.07433 18.268ZM12.4743 14.068L15.6743 11.968L15.1257 11.132L11.9257 13.232L12.4743 14.068ZM18.8743 9.86803L22.0743 7.76803L21.5257 6.93198L18.3257 9.03198L18.8743 9.86803ZM25.2743 5.66802L28.4743 3.56803L27.9257 2.73198L24.7257 4.83198L25.2743 5.66802ZM33 1.58349e-06L21.4719 0.659641L27.8072 10.3135L33 1.58349e-06ZM1.54866 21.836L3.14866 20.786L2.05134 19.114L0.451344 20.164L1.54866 21.836ZM6.34866 18.686L9.54866 16.586L8.45134 14.914L5.25134 17.014L6.34866 18.686ZM12.7487 14.486L15.9487 12.386L14.8513 10.714L11.6513 12.814L12.7487 14.486ZM19.1487 10.286L22.3487 8.18605L21.2513 6.51395L18.0513 8.61395L19.1487 10.286ZM25.5487 6.08605L28.7487 3.98605L27.6513 2.31395L24.4513 4.41395L25.5487 6.08605Z"
-										fill="#FB910B"
-									/>
-								</svg>
-								<svg
-									class="service__apply__arrow--m"
-									xmlns="http://www.w3.org/2000/svg"
-									width="12"
-									height="19"
-									viewBox="0 0 12 19"
-									fill="none"
-								>
-									<path
-										d="M6 -7.7486e-07L3.11325 5L8.88675 5L6 -7.7486e-07ZM6.5 19L6.5 18.05L5.5 18.05L5.5 19L6.5 19ZM6.5 16.15L6.5 14.25L5.5 14.25L5.5 16.15L6.5 16.15ZM6.5 12.35L6.5 10.45L5.5 10.45L5.5 12.35L6.5 12.35ZM6.5 8.55L6.5 6.65L5.5 6.65L5.5 8.55L6.5 8.55ZM6.5 4.75L6.5 2.85L5.5 2.85L5.5 4.75L6.5 4.75ZM6 -7.7486e-07L0.226498 10L11.7735 10L6 -7.7486e-07ZM7 19L7 18.05L5 18.05L5 19L7 19ZM7 16.15L7 14.25L5 14.25L5 16.15L7 16.15ZM7 12.35L7 10.45L5 10.45L5 12.35L7 12.35ZM7 8.55L7 6.65L5 6.65L5 8.55L7 8.55ZM7 4.75L7 2.85L5 2.85L5 4.75L7 4.75Z"
-										fill="#FB910B"
-									/>
-								</svg>
-								<div class="service__apply__extend">
+								<!-- <div class="service__apply__extend">
 									<div class="service__apply__extendbody">
 										<div class="service__apply__extendicon">
 											<img
@@ -139,7 +74,7 @@ onUnmounted(() => {
 										</div>
 									</div>
 									<h6 class="service__apply__extendhead">送件</h6>
-								</div>
+								</div> -->
 							</div>
 							<h5 class="service__apply__head">簽約</h5>
 						</li>
@@ -206,7 +141,6 @@ onUnmounted(() => {
 									/>
 								</div>
 								<span class="service__apply__num">9</span>
-								<span class="service__apply__end">END</span>
 							</div>
 							<h5 class="service__apply__head">保固維運</h5>
 						</li>
