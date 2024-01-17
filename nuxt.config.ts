3acab9c3b82e39128f55c86ab572c6bc29b01e81
yaj55billy <yaj55ushydna@gmail.com>
2024-01-17 20:23:33 +0800

    deploy set

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,7 @@ export default defineNuxtConfig({
 						"湧業能源成立於2020年，主力太陽能系統建置與投資開發之相關領域。致力於以太陽能光電協助企業邁向永續能源轉型，為台灣2050年淨零排碳目標盡一份心力。我們為業主提供可靠、永續的全方位光電解決方案，公司經營項目包含：光電工程EPC、電廠維運管理、電廠投資以及儲能系統規劃建置。為台灣打造乾淨永續的綠能未來。",
 				},
 				{ property: "og:image", content: "~/assets/img/meta.jpg" },
-				// { property: "og:url", content: "https://www.gush-energy.com" },
+				{ property: "og:url", content: "https://www.gush-energy.com" },
 			],
 			link: [
 				{
